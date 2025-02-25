e1783e8f87d72ddd3a84177849806f7b7bd93ae3
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-02-25 11:02:51 +0100

    dfghjk

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
+let userChoice = "age"; 
 
+let user = {
+    name: 'Christoffer',
+    age: 21,
+    shirt_color: 'purple',
+    education: 'multimediadesigner',
+    isStudent: true,
+    adress: {
+        street: 'BlaBlaVej',
+        street_number: 22,
+        city: 'Aalborg',   
+        zipcode: 9000
+    },
+    pets: ['dog', 'cat', 'fish'],
+};   
+
+user["Sibling"] = ['Camilla']
+user.isReligious = false;
+
+//sådan kan man loope igennem et objects properties
+for (let key in user) {
+    let value = user[key];
+    console.log(key + ": " + value);
+}
+
+console.table(user);
+console.log(user.name);
+console.log(user[userChoice]);
